930ea3df5e978c86c47fc94b850917ac4d57379a
Lucas Mortensen <lucas37c@gmail.com>
2025-03-12 14:36:05 +0100

    initiates base game setup

---
+++ play.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.flip()
+
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
